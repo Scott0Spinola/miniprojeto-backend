a0ff54e15935e3e2b8c781f8dd4fbfb69bac584b
Scott0Spinola <spinolascott17@gmail.com>
2025-04-18 21:43:18 +0100

    Parte B -
     new stuff

--- server.js
+++ server.js
@@ -1,34 +1,27 @@
-const express = require('express');
-const mysql = require('mysql');
-const nodemon = require('nodemon');
+const express = require("express");
+const mysql = require("mysql");
+const nodemon = require("nodemon");
 const app = express();
 const port = 3001;
-
+app.use(express.json());
 const connection = mysql.createConnection({
-  host:"localhost",
-  user:"root",
-  password:"password",
-  database:"mini_projeto",
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "mini_projeto",
 });
 
-
-
-app.get('/test', (req, res) =>{
-  res.send('It is Working!');
+app.get("/test", (req, res) => {
+  res.send("It is Working!");
 });
-
-
 
 // PARTE B
-app.get('/product/:id',(req, res) =>{
+app.get("/product/:id", (req, res) => {
   const id = req.params.id;
-  try { 
-    connection.query('SELECT * FROM products WHERE id=?', [id], (err, rows) => {
-      if (err) {
-        return res.status(404).json({message:'errorrr'});
-      }
-      if(rows === 0){
-        return res.status(404).json({message:"id nao foi encontrado!!!"});
+  try {
+    connection.query("SELECT * FROM products WHERE id=?", [id], (err, rows) => {
+      if (rows.length === 0) {
+        return res.status(404).json({ message: "Product with id " + id + " not found!!" });
       }
       const product = rows[0];
       res.status(200).json({
@@ -36,29 +29,67 @@ app.get('/product/:id',(req, res) =>{
         name: product.name,
         barcode: product.barcode,
         department: product.department,
+        description: product.description,
         review: product.review,
         weight: product.weight,
         price: product.price,
-        comment: product.comment
+        comment: product.comment,
       });
     });
   } catch (err) {
-    res.status(500).json({message:"Erro no servidor!!!!"});
+    res.status(500).json({ message: "Erro no server!!!" });
   }
 });
-/*
+
 app.delete('/product/:id', (req, res) =>{
   const id = req.params.id;
   try {
-    connection.query('DELETE FROM products WHERE id=?',[id], (err, result) =>{
-      if(err){
-        return res.status(500).json('')
+    connection.query("SELECT * FROM products WHERE id=?", [id], (err, rows) => {
+      if (!(rows.length === 0)) { 
+        try {
+          connection.query("DELETE FROM products WHERE id=?", [id], (err, rows) => {
+            res.status(200).json({ message: "Product with id " + id + " was deleted!!" });
+          });
+        } catch (err) {
+          res.status(500).json({ message: "Erro no server!!!" });
+        }
+      }else{
+        return res.status(404).json({ message: "Product with id " + id + " not found!!" });
       }
-      if ()
-    })
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Erro no server!!!" });
   }
-})
-*/
+});
+
+app.post('/productSearch', (req, res) =>{
+  const keyWord = req.body.word;
+  var guardarId = [];
+  try{
+    connection.query("SELECT id, description FROM products", (err, res) =>{
+      for (let index = 0; index < res.length; index++) {
+        var element = res[index].description.toLowerCase();
+        console.log(element);
+        if(element.includes(keyWord)){
+          guardarId.push(res[index].id);
+        }
+      }
+    })
+    try {
+        connection.query("SELECT * FROM products WHERE ")
+    } catch (error) {
+      
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Erro no server!!!" });
+  }
+});
+
+
+
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
